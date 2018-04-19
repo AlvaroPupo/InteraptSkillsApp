dad27ceff0e71565da6ad3eff1e7f7b2c56f0770
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-04-19 13:06:43 -0400

    getting started !!

--- app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/MainActivity.java
+++ app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.juanalvaropupo.interaptskillsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
