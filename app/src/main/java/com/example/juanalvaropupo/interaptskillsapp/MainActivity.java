eeaa2ba0e6d1b25eb52835417e6674bbc6d65070
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-04-27 16:08:35 -0400

    added alertdialog to floating action button

--- app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/MainActivity.java
+++ app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,14 +38,6 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/Tab1.java
+++ app/src/main/java/com/example/juanalvaropupo/interaptskillsapp/Tab1.java
@@ -1,23 +1,34 @@
 package com.example.juanalvaropupo.interaptskillsapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Tab1 extends Fragment {
 
     private RecyclerView recyclerView;
     private List<Students> studentsList;
     private AdapterRecyclerView adapterRecyclerView;
+    private FloatingActionButton fab;
+    private EditText dialogEditext;
 
     @Nullable
     @Override
@@ -39,7 +50,42 @@ public class Tab1 extends Fragment {
         super.onStart();
         recyclerView = getActivity().findViewById(R.id.recyclerView);
         studentsList = new ArrayList<>();
+
+        dialogEditext = getActivity().findViewById(R.id.dialog_edittext);
+        fab = getActivity().findViewById(R.id.fab);
+        setOnClickListener();
         setUpStudentsList();
+    }
+
+    private void setOnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View subview = LayoutInflater.from(getActivity()).inflate(R.layout.add_game_dialog_layout, null, false);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Add a new student to the class")
+                        .setView(subview)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+//                                String studentNameInput = dialogEditext.getText().toString();
+//
+//                                Students students = new Students(studentNameInput);
+//                                studentsList.add(students);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 
     private void setUpStudentsList() {
@@ -53,4 +99,7 @@ public class Tab1 extends Fragment {
         adapterRecyclerView.notifyDataSetChanged();
     }
 
-}
+
+    }
+
+
